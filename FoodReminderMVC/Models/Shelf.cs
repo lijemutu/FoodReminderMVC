b776e5534dee6f314f8df2c8c545a7bf1cd8272b
lijemutu <erlowarlo@gmail.com>
2021-07-26 19:59:34 -0500

    mongodb integration /models

---
+++ FoodReminderMVC/Models/Shelf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace FoodReminderMVC.Models
+{
+    public class Shelf
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Capacity")] 
+        public int Capacity { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public Product[] Products{ get; set; }
+
+        
+    }
+}
--- FoodReminderMVC/Program.cs
+++ FoodReminderMVC/Program.cs
@@ -4,8 +4,13 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
 
 namespace FoodReminderMVC
 {
@@ -13,7 +18,23 @@ namespace FoodReminderMVC
     {
         public static void Main(string[] args)
         {
+            MongoClient dbClient = new MongoClient("mongodb://localhost");
+            var dbList = dbClient.ListDatabases().ToList();
+            var database = dbClient.GetDatabase("sample_training");
+            var collection = database.GetCollection<BsonDocument>("grades");
+            var document = new BsonDocument { { "student_id", 10000 }, {
+                    "scores",
+                    new BsonArray {
+                        new BsonDocument { { "type", "exam" }, { "score", 88.12334193287023 } },
+                        new BsonDocument { { "type", "quiz" }, { "score", 74.92381029342834 } },
+                        new BsonDocument { { "type", "homework" }, { "score", 89.97929384290324 } },
+                        new BsonDocument { { "type", "homework" }, { "score", 82.12931030513218 } }
+                    }
+                }, { "class_id", 480 }
+            };
+            collection.InsertOne(document);
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
