3e8344c00d4caa9ba6037b9a8020b106cc80f5b4
lijemutu <erlowarlo@gmail.com>
2021-07-27 22:43:12 -0500

    Product Class added

--- FoodReminderMVC/Models/Product.cs
+++ FoodReminderMVC/Models/Product.cs
@@ -7,5 +7,12 @@ namespace FoodReminderMVC.Models
 {
     public class Product
     {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int? ProductListId { get; set; }
+
     }
 }
